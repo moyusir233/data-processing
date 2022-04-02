4c03ddbfb7b23dc16ed0c249017a61e8b184d82f
moyu <279972161@qq.com>
2022-04-02 16:21:19 +0800

    修复了转换查询influxdb得到的数据的格式时的一些问题: 1. 查询警告时deviceClassID没有封装到deviceClassID中 2. 封装设备状态信息的tag时的if判断错误

--- internal/data/repo.go
+++ internal/data/repo.go
@@ -182,7 +182,7 @@ func (r *Repo) BatchGetDeviceStateInfo(deviceClassID int, option *biz.QueryOptio
 			// 解析tag
 			for k, v := range record.Values() {
 				// 除了系统字段、table字段以及deviceClassID字段，其余都视作tag
-				if !strings.HasSuffix(k, "_") && k != "deviceClassID" && k != "table" {
+				if !strings.HasPrefix(k, "_") && k != "deviceClassID" && k != "table" {
 					result[pos].Tags[k] = fmt.Sprintf("%v", v)
 				}
 			}
@@ -246,6 +246,7 @@ func (r *Repo) GetWarningMessage(option *biz.QueryOption) ([]*utilApi.Warning, e
 		if warnings[pos].DeviceId == "" {
 			warnings[pos].DeviceId = record.Measurement()
 			warnings[pos].DeviceFieldName = record.ValueByKey("deviceFieldName").(string)
+			warnings[pos].DeviceClassId = int32(record.ValueByKey("deviceClassID").(int))
 		}
 
 		// 解析field，field包括start、end以及警告信息message
